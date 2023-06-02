73374857ab0eaca44631e4300d6e1431859b7d28
gluisjulian <gluisjulian@gmail.com>
2023-06-02 16:54:31 -0300

    Adicionado pacotes Nuget e Context

---
+++ GeekShopping/GeekShopping.ProductAPI/Model/Context/GeekShoppingContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class GeekShoppingContext : DbContext
+    {
+        public GeekShoppingContext(DbContextOptions<GeekShoppingContext> options): base(options)
+        {
+            
+        }
+
+
+    }
+}
--- GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -1,6 +1,10 @@
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<GeekShoppingContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("GeekShoppingConnectionString")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
