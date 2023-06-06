8772a9cb215ff9438ff66e57d2598a23e51f6e17
gluisjulian <gluisjulian@gmail.com>
2023-06-06 16:22:11 -0300

    Criação das migrations.

---
+++ GeekShopping/GeekShopping.ProductAPI/Migrations/20230606185327_AdicionadaTabelaProduto.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeekShopping.ProductAPI.Migrations
+{
+    public partial class AdicionadaTabelaProduto : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "produto",
+                columns: table => new
+                {
+                    id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    nome = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    preco = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    descricao = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    nome_categoria = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    imagem_url = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_produto", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "produto");
+        }
+    }
+}
