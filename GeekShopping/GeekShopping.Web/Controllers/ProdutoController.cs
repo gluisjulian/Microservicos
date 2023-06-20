1018815a3fb79de4ac97ee54852e47b45101d8df
gluisjulian <gluisjulian@gmail.com>
2023-06-20 20:22:46 -0300

    Criação da pagina index de produto

---
+++ GeekShopping/GeekShopping.Web/Controllers/ProdutoController.cs
@@ -0,0 +1,22 @@
+﻿using GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeekShopping.Web.Controllers
+{
+    public class ProdutoController : Controller
+    {
+
+        private readonly IProdutoService _produtoService;
+
+        public ProdutoController(IProdutoService produtoService)
+        {
+            _produtoService = produtoService ?? throw new ArgumentNullException(nameof(produtoService));
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var produtos = await _produtoService.GetAllProdutos();
+            return View(produtos);
+        }
+    }
+}
